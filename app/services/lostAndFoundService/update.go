191b182681cff151a223d91073b26fb6fa07e777
zzb <1072396779@qq.com>
2024-11-22 00:51:30 +0800

    chore:修改审核失物招领数据库操作

--- app/services/lostAndFoundService/update.go
+++ app/services/lostAndFoundService/update.go
@@ -8,9 +8,9 @@ import (
 // ReviewLostAndFound 审核失物招领
 func ReviewLostAndFound(recordId uint, isApproved bool) error {
 	if isApproved == true {
-		result := database.DB.Model(&models.LostAndFoundRecord{}).Where("id = ?", recordId).Update("is_approved", 1)
+		result := database.DB.Model(&models.LostAndFoundRecord{}).Where("id = ?", recordId).Updates(map[string]interface{}{"is_approved": 1, "is_processed": 2})
 		return result.Error
 	}
-	result := database.DB.Model(&models.LostAndFoundRecord{}).Where("id = ?", recordId).Update("is_approved", 0)
+	result := database.DB.Model(&models.LostAndFoundRecord{}).Where("id = ?", recordId).Updates(map[string]interface{}{"is_approved": 0, "is_processed": 1})
 	return result.Error
 }
