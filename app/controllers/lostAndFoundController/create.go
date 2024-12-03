963203c0228fbebd0dae558c9dbe69e5a140b088
zzb <1072396779@qq.com>
2024-12-03 19:09:19 +0800

    refactor: 优化创建失物招领接口的入参和图片url存储方式

--- app/controllers/lostAndFoundController/create.go
+++ app/controllers/lostAndFoundController/create.go
@@ -9,15 +9,16 @@ import (
 )
 
 type createLostAndFoundData struct {
-	Type         bool   `json:"type"`                            // 1-失物 0-寻物
-	Name         string `json:"name" binding:"required"`         // 物品名称
-	Introduction string `json:"introduction" binding:"required"` // 物品介绍
-	Campus       uint8  `json:"campus" binding:"required"`       // 校区 1-朝晖 2-屏峰 3-莫干山
-	Kind         uint8  `json:"kind"`                            // 物品种类 1其他2证件3箱包4首饰5现金6电子产品7钥匙
-	Place        string `json:"place" binding:"required"`        // 丢失或拾得地点
-	Time         string `json:"time" binding:"required"`         // 丢失或拾得时间
-	Imgs         string `json:"imgs" binding:"required"`         // 物品图片，多个图片以逗号分隔
-	Contact      string `json:"contact" binding:"required"`      // 联系方式
+	Type         bool     `json:"type"`                            // 1-失物 0-寻物
+	Name         string   `json:"name" binding:"required"`         // 物品名称
+	Introduction string   `json:"introduction" binding:"required"` // 物品介绍
+	Campus       uint8    `json:"campus"`                          // 校区 0-其他 1-朝晖 2-屏峰 3-莫干山
+	Kind         uint8    `json:"kind"`                            // 物品种类 1其他2证件3箱包4首饰5现金6电子产品7钥匙
+	Place        string   `json:"place" binding:"required"`        // 丢失或拾得地点
+	Time         string   `json:"time" binding:"required"`         // 丢失或拾得时间
+	Imgs         []string `json:"imgs" binding:"required"`         // 物品图片，多个图片以逗号分隔
+	Contact      string   `json:"contact" binding:"required"`      // 联系方式
+	ContactWay   uint8    `json:"contact_way" binding:"required"`  // 联系方式选项 1-手机号 2-qq 3-微信 4-邮箱
 }
 
 // CreateLostAndFound 创建一条失物招领
@@ -29,6 +30,15 @@ func CreateLostAndFound(c *gin.Context) {
 		return
 	}
 
+	// 判断imgs是否大于9
+	if len(data.Imgs) > 9 {
+		apiException.AbortWithException(c, apiException.ParamError, nil)
+		return
+	}
+
+	// 将[]string转为string
+	imgs := utils.StringsToString(data.Imgs)
+
 	err = lostAndFoundService.SaveLostAndFound(models.LostAndFoundRecord{
 		Type:         data.Type,
 		Name:         data.Name,
@@ -37,9 +47,10 @@ func CreateLostAndFound(c *gin.Context) {
 		Kind:         data.Kind,
 		Place:        data.Place,
 		Time:         data.Time,
-		Imgs:         data.Imgs,
+		Imgs:         imgs,
 		Publisher:    utils.GetUser(c).StudentID,
 		Contact:      data.Contact,
+		ContactWay:   data.ContactWay,
 		IsProcessed:  2,
 		IsApproved:   2,
 	})
