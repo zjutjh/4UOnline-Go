3058fab16247c38b6f35da056c5aff338548e88c
zzb <1072396779@qq.com>
2024-12-11 22:04:23 +0800

    refactor(app/utils): 优化 StringsToString 函数
    - 将原有的手动拼接字符串逻辑替换为 strings.Join 函数

--- app/utils/converter.go
+++ app/utils/converter.go
@@ -4,14 +4,7 @@ import "strings"
 
 // StringsToString 将字符串数组转换为字符串,以逗号分隔
 func StringsToString(strSlice []string) string {
-	var result string
-	for i, str := range strSlice {
-		if i != 0 {
-			result += ","
-		}
-		result += str
-	}
-	return result
+	return strings.Join(strSlice, ",")
 }
 
 // StringToStrings 将字符串转换为字符串数组
